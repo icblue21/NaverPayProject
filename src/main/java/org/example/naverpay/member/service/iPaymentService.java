d8e08e56c1038453b667472bb5c1f4b37b0bfeec
ykim-yeji <ykim5867@gmail.com>
2022-11-11 11:24:01 +0900

    fix: iPaymentService pull한 후 기존 getPaymentInfo에 삭제 기능 추상메소드만 추가

--- src/main/java/org/example/naverpay/member/service/iPaymentService.java
+++ src/main/java/org/example/naverpay/member/service/iPaymentService.java
@@ -1,15 +1,9 @@
 package org.example.naverpay.member.service;
 
 import org.example.naverpay.member.dto.PaymentDTO;
-import org.example.naverpay.member.dto.ShoppingDTO;
-
-import javax.servlet.http.HttpSession;
 
 public interface iPaymentService {
 
-    public boolean isLogin(HttpSession session);
-    public boolean isPurchaseHistory(String sId);
-    public PaymentDTO orderStatus(String sId);
-    public boolean isYourProduct(HttpSession session, ShoppingDTO shoppingDTO);
-    public boolean isOrderStatusRemoved(String pId);
+    PaymentDTO getPaymentInfo(String sId);
+    boolean isPaymentInfoRemoved(String sId);
 }
